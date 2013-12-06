d4aff1506d3081928055c3ff06b0e39317fee910
Satish Goda <satishgoda@gmail.com>
2013-12-07 01:32:54 +0530

    Added additional behavior to Poll
    >>> poll.choice_set.filter()[0].delete()
    >>> poll.choice_set.count()
    0
    
    >>> for choice in poll.choice_set.all():
    ...   print Poll.objects.get(pk=choice.poll.id).question,
    choice.choice_text
    ...
    Which 3D software did you like? Blender
    Which 3D software did you like? Houdini

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
+
 # Create your models here.
 
 class Poll(models.Model):
@@ -8,6 +11,9 @@ class Poll(models.Model):
     
     def __unicode__(self):
         return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
