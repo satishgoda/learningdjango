5d7adbc5dad6150ea5ba91b3f480288371a5de19
Satish Goda <satishgoda@gmail.com>
2013-12-07 16:20:14 +0530

    Made Choice inline with a Poll
    By making the Choice model inline within PollAdmin, Django will create a
    stack of Choices for the staff/superuser to create.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,12 +3,17 @@ from polls.models import Poll, Choice
 
 # Customize admin UI for models
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = []
     fieldsets.append((None, {'fields': ['question']}));
     fieldsets.append(('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}));
 
+    inlines = [ChoiceInline]
+
 # Register your models here.
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
