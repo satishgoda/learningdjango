26638bfa6b6e1784ff0c888ecfbc5c4f29bfeef2
Satish Goda <satishgoda@gmail.com>
2013-12-07 12:54:58 +0530

    Customized the admin form
    By default the field order is the order of instantiation in the Model
    definition.
    
    But by subclassing admin.ModelAdmin, we can specify a custom order to
    the fields by declaring a fields class variable.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from polls.models import Poll
 
+# Customize admin UI for models
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
 # Register your models here.
 
-admin.site.register(Poll)
+admin.site.register(Poll, PollAdmin)
+
