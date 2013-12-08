bfca60502db60cc3bc3b6090da1baad3bedfd25b
Satish Goda <satishgoda@gmail.com>
2013-12-08 13:06:54 +0530

    Hook the root URLconf with the polls.views.index
    Also fixed some syntax errors that ensued because of some foreign
    characters.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,10 +8,10 @@ def index(request):
     return HttpResponse("Hello World! Time to poll baby.")
 
 # https://docs.djangoproject.com/en/1.6/intro/tutorial03/
-# A view is a “type” of Web page in your Django application that generally 
+# A view is a "type" of Web page in your Django application that generally 
 # serves a specific function and has a specific template.
 #
 # In Django, web pages and other content are delivered by views. Each view is 
 # represented by a simple Python function (or method, in the case of class-based views). 
-# Django will choose a view by examining the URL that’s requested (to be precise, the 
+# Django will choose a view by examining the URL that's requested (to be precise, the 
 # part of the URL after the domain name).
