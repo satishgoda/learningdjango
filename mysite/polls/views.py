f18783f152998ab44057168a36d681ac66f9a9f7
Satish Goda <satishgoda@gmail.com>
2013-12-11 00:29:34 +0530

    Form for details view
    After a loooooooooooooooong time i was typing the form, input, label
    HTML tags.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from polls.models import Poll
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Poll, Choice
 
 # Create your views here.
 
@@ -14,25 +12,30 @@ def index(request):
 
 def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id) 
-    return render(request, 'polls/detail.html', {'poll': poll})
+    context = {'poll': poll}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, poll_id):
-    results = Poll.objects.filter(pk=poll_id)
-    output = ''
-    if not results:
-        output = "The id ({0}) does not exist in the database".format(poll_id)
-    else:
-        poll = results[0]
-        output = "<b>{0}</b><br>\n".format(poll.question)
-        output += "<ul>\n"
-        for choice in poll.choice_set.filter():
-            output += '<li>{0}: {1}</li><br>\n'.format(choice.choice_text, choice.votes)
-        output += "</ul>\n"
-    return HttpResponse(output)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    context = {'poll': poll}
+    return render(request, 'polls/results.html', context)
     
 def vote(request, poll_id):
-    return HttpResponse("Note yet implemented")
-    
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll details form
+        context = {'poll': poll, 'error_message': "You didn't select a choice"}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with
+        # POST data. This prevents data from being psoted twice if a user hits the
+        # Back button
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
+
 # https://docs.djangoproject.com/en/1.6/intro/tutorial03/
 # A view is a "type" of Web page in your Django application that generally 
 # serves a specific function and has a specific template.
