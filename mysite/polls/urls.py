72d747d3ddbf9fb277953945d1403aef21266e44
Satish Goda <satishgoda@gmail.com>
2013-12-11 01:26:47 +0530

    Fixed error resulting from previous commit
    Generic Views are the bomb.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,7 +7,7 @@ from polls import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<poll_id>\d+)/detail/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/detail/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 )
