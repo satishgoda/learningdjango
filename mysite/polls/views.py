52ecdd07393b92dc32fd116f41809e4cd82ddf5b
Satish Goda <satishgoda@gmail.com>
2013-12-18 00:35:14 +0530

    Tutorial 5 on testing is done
    I faced some problems where two of the tests were failing. After some
    tense moments, I realized that there was a mistake on my part in the
    view where i did not put a . at the end of a message. In my test case
    there was a dot. So my tests were failing since the assertions were not
    getting through the test runner. Lesson learned.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,6 +19,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Poll
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any polls that aren't published yet.
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
     
 def vote(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
