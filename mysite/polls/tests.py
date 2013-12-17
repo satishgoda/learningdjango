4d3def40c2447c9fe509bc0dd30fb3aedb543a10
Satish Goda <satishgoda@gmail.com>
2013-12-17 23:42:14 +0530

    Added few more test cases to solidify Poll model operation

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -10,3 +10,12 @@ class PollMethodTests(TestCase):
     def test_was_published_recently_with_future_poll(self):
         future_poll = Poll(pub_date=timezone.now()+datetime.timedelta(days=30))
         self.assertEqual(future_poll.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_poll(self):
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+        
+    def test_was_published_recently_with_recent_poll(self):
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
+
